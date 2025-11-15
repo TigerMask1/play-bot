a5dbe3f31289046b6fe48890c20542b897508e14
TigerMask1 <tigermask757@gmail.com>
2025-11-15 05:50:14 +0530

    Update progressBar.js

--- progressBar.js
+++ progressBar.js
@@ -14,25 +14,18 @@ function createProgressBar(current, max, length = 20, showPercentage = true) {
   return bar;
 }
 
-function createLevelProgressBar(currentTokens, requiredTokens) {
-  const percentage = Math.min(100, (currentTokens / requiredTokens) * 100);
-  const filledLength = Math.round((percentage / 100) * 8);
-  const emptyLength = 8 - filledLength;
-
-  // Choose bar colour based on percentage
-  let color = "\u001b[32m"; // green
-  if (percentage < 25) color = "\u001b[31m";             // red
-  else if (percentage < 50) color = "\u001b[38;5;208m";  // orange
-  else if (percentage < 75) color = "\u001b[33m";        // yellow
-
-  const reset = "\u001b[0m";
-
-  const filled = color + "█" + reset;
-  const empty = "░"; // grey
-
-  const bar = filled.repeat(filledLength) + empty.repeat(emptyLength);
-
-  return `[${bar}] ${currentTokens}/${requiredTokens} 🎫`;
+function createColoredProgressBar(current, max, length = 20) {
+  const percentage = Math.min(100, Math.max(0, (current / max) * 100));
+  const filledLength = Math.round((percentage / 100) * length);
+  const emptyLength = length - filledLength;
+  
+  let color = '🟩';
+  if (percentage < 25) color = '🟥';
+  else if (percentage < 50) color = '🟧';
+  else if (percentage < 75) color = '🟨';
+  
+  const bar = color.repeat(filledLength) + '⬜'.repeat(emptyLength);
+  return `${bar} ${current}/${max}`;
 }
 
 function createQuestProgressBar(current, max) {
